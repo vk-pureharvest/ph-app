01333b26e8f1ffb4b5776db95f497ae9cfecbdcb
vk-pureharvest <84262862+vk-pureharvest@users.noreply.github.com>
2022-03-04 15:10:06 +0400

    updated customer list complaints form

--- resources/views/complaints/edit.blade.php
+++ resources/views/complaints/edit.blade.php
@@ -158,6 +158,7 @@ div {
       <option value="TFC FRUITS TRADING LLC">{{"TFC FRUITS TRADING LLC"}}</option>
       <option value="TRANSMED OVERSEAS INCORPORATED S.A.">{{"TRANSMED OVERSEAS INCORPORATED S.A."}}</option>
       <option value="T.V. VIJAYAN">{{"T.V. VIJAYAN"}}</option>
+      <option value="URBAN FOODS LLC">{{"URBAN FOODS LLC"}}</option>
       <option value="VEGBERRY MIDDLE EAST FRUITS & VEGETABLES TRADING LLC">{{"VEGBERRY MIDDLE EAST FRUITS & VEGETABLES TRADING LLC"}}</option>
       <option value="VIP SAMPLE BOX">{{"VIP SAMPLE BOX"}}</option>
    </select>   
