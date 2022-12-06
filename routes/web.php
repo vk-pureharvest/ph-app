80395a55b473593cc51b75f0b89b3da55003c714
vk-pureharvest <84262862+vk-pureharvest@users.noreply.github.com>
2022-12-06 17:21:10 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,10 @@ Route::get('/', function () {
 Auth::routes();
 
 
-Route::group(['middleware' => ['auth','verified']], function () {
+// Route::group(['middleware' => ['auth','verified']], function () {
     
     Route::get('register', 'HomeController@index')->name('register');
-});
+// });
 
 //Route::get('/multi-insert', [BrixesController::class, 'index'])->name('multi-insert');
 //Route::get('/submitData', [BrixesController::class, 'submitData'])->name('submitData');
