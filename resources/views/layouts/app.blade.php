a615c8b94b19ca29524e271a63d32c00d3c01387
vk-pureharvest <84262862+vk-pureharvest@users.noreply.github.com>
2022-12-06 15:23:14 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,9 +27,9 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                             </li>
-                            <li class="nav-item">
+                            <!-- <li class="nav-item">
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
+                            </li> -->
                         @else
                         @if(Auth::user()->role_id==1)
                         <li class="nav-item dropdown">
