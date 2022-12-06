02ca7b573c9fbe8a57344c5d5666cb0d4a646af9
vk-pureharvest <84262862+vk-pureharvest@users.noreply.github.com>
2022-12-06 14:18:18 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,7 +73,7 @@
                         <a href="{{ url('/home') }}">Home</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <!-- <a href="{{ route('register') }}">Register</a> -->
                     @endauth
                 </div>
             @endif
